f25de1deb52bee396b8571c777e4a71d669d401c
David Gyesi BINEY <gyesibiney@users.noreply.huggingface.co>
2023-08-14 21:17:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,57 @@
-from fastapi import FastAPI,Query
-from pydantic import BaseModel
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
+import json
 import joblib
 
-# instantiating the app
-app= FastAPI()
+app = FastAPI()
 
-class patient_item(BaseModel):
-    prg: float = Query(..., description="Plasma glucose"),
-    pl: float = Query(..., description="Blood Work Result-1 (mu U/ml)"),
-    pr: float = Query(..., description="Blood Pressure (mm Hg)"),
-    sk: float = Query(..., description="Blood Work Result-2 (mm)"),
-    ts: float = Query(..., description="Blood Work Result-3 (mu U/ml)"),
-    m11: float = Query(..., description="Body mass index (weight in kg/(height in m)^2"),
-    bd2: float = Query(..., description="Blood Work Result-4 (mu U/ml)"),
-    age: int = Query(..., description="Patient's age (years)")   
+# Load the XGBoost model
+xgb_model = joblib.load("xgb_model.joblib")
 
-model= joblib.load('xgb_model.joblib')
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to my Sepsis-Prediction-APP-using-FastAPI"}
 
-@app.get('/')
-async def patient_endpoint(item:patient_item):
-    df= pd.DataFrame([item.dict().values], columns= item.dict().keys())
-    yhat= model.predict(df)
-    return{'prediction':int(yhat)} 
-    
-    
+@app.post("/predict/")
+async def predict_sepsis(request: Request):
+    try:
+        # Get request JSON body
+        request_data = await request.json()
+
+        # Extract input features from request body
+        prg = request_data.get("prg")
+        pl = request_data.get("pl")
+        pr = request_data.get("pr")
+        sk = request_data.get("sk")
+        ts = request_data.get("ts")
+        m11 = request_data.get("m11")
+        bd2 = request_data.get("bd2")
+        age = request_data.get("age")
+
+        # Prepare input features for prediction
+        input_features = [prg, pl, pr, sk, ts, m11, bd2, age]
+
+        # Make predictions using the loaded model
+        prediction = xgb_model.predict([input_features])[0]
+
+        # Determine the prediction outcome message
+        if prediction == 0:
+            prediction_message = "Patient has no sepsis"
+        else:
+            prediction_message = "Patient has sepsis"
+
+        # Create a response dictionary
+        response = {
+            "request": {
+                # ... input values ...
+            },
+            "prediction": {
+                "class_0_probability": prediction[0],
+                "class_1_probability": prediction[1],
+                "prediction_message": prediction_message
+            }
+        }
+
+        return JSONResponse(content=response, media_type="application/json")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
