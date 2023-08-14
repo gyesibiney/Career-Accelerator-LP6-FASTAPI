55a778a06588c25a8fa2146d855f21ad574cd167
David Gyesi BINEY <gyesibiney@users.noreply.huggingface.co>
2023-08-14 17:28:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-from fastapi import FastAPI, Query, Request, HTTPException
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-import joblib
+from fastapi import fastAPI
+from pydantic import BaseModel
 
-app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+# instantiating the app
 
-# Load the XGBoost model
-xgb_model = joblib.load("xgb_model.joblib")
+app= fastAPI()
 
-@app.get("/")
-async def read_root():
-    return {"message": "Welcome to my Sepsis-Prediction-APP-using-FastAPI"}
-
-@app.get("/predict/")
-async def predict_sepsis(
-    request: Request,
+class patient_item(BaseModel):
     prg: float = Query(..., description="Plasma glucose"),
     pl: float = Query(..., description="Blood Work Result-1 (mu U/ml)"),
     pr: float = Query(..., description="Blood Pressure (mm Hg)"),
@@ -23,47 +13,14 @@ async def predict_sepsis(
     ts: float = Query(..., description="Blood Work Result-3 (mu U/ml)"),
     m11: float = Query(..., description="Body mass index (weight in kg/(height in m)^2"),
     bd2: float = Query(..., description="Blood Work Result-4 (mu U/ml)"),
-    age: int = Query(..., description="Patient's age (years)")
-):
-    try:
-        # Prepare input features for prediction
-        input_features = [prg, pl, pr, sk, ts, m11, bd2, age]
+    age: int = Query(..., description="Patient's age (years)")   
 
-        # Make predictions using the loaded model
-        prediction = xgb_model.predict([input_features])[0]
+model= joblib.load('xgb_model.joblib')
 
-        # Determine the prediction outcome and create a response message
-        if prediction == 0:
-            prediction_message = "Patient has no sepsis"
-        else:
-            prediction_message = "Patient has sepsis"
-
-        # Create a JSON response
-        response = {
-                "request": {
-                "prg": prg,
-                "pl": pl,
-                "pr": pr,
-                "sk": sk,
-                "ts": ts,
-                "m11": m11,
-                "bd2": bd2,
-                "age": age
-            },
-            "prediction": {
-                "class_0_probability": prediction[0],
-                "class_1_probability": prediction[1],
-                "prediction_message": prediction_message  # Add prediction message to response
-            }
-        }
-
-        return templates.TemplateResponse(
-            "display_params.html",
-            {
-                "request": request,
-                "input_features": response["request"],
-                "prediction": response["prediction"]
-            }
-        )
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+@app.get('/')
+async def patient_endpoint(item:patient_item):
+    df= pd.DataFrame([item.dict().values], columns= item.dict().keys())
+    yhat= model.predict(df)
+    return{'prediction':int(yhat)} 
+    
+    
